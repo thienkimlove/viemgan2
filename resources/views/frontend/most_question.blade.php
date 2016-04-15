83a5c6f284cf932daafdb37b663c7e766019ea19
Đỗ Mạnh Quân <manhquan.do@ved.com.vn>
2016-04-15 11:53:18 +0700

    fix

--- resources/views/frontend/most_question.blade.php
+++ resources/views/frontend/most_question.blade.php
@@ -13,15 +13,6 @@
                             <img src="{{url('images/question.png')}}" alt="">
                         </a>
                     </div>
-                @foreach ($mostQuestions as $question)
-                    <div class="list-medicine">
-                        <a href="{{url('hoi-dap')}}" class="thumb">
-                            <img src="{{url('files/images/100_' . $question->image)}}" width="115" height="80" alt="">
-                        </a>
-
-                        <h3><a href="{{url('hoi-dap')}}">{{str_limit($question->question, 40)}}</a></h3>
-                    </div>
-                @endforeach
             </div>
         </div>
     </div>
